f3d88b2f0b3c6c6932a0b7418205d14e10d09f66
NickHaggerty <Nickhaggerty28@gmail.com>
2020-09-16 11:28:56 -0600

    updated vies and urls.py

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -5,10 +5,13 @@ from .views import (
     BlogDetailView,
     BlogCreateView,
     BlogUpdateView,
+    BlogDeleteView,
     )
 
 
 urlpatterns = [
+    path('post/<int:pk>/delete/',
+         BlogDeleteView.as_view(), name = 'post_delete'),
     path('post/<int:pk>/edit/',
          BlogUpdateView.as_view(), name='post_edit'),
     path('post/new/', BlogCreateView.as_view(), name='post_new'),
